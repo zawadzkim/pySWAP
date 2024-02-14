10c2b76c909945511de4b77e9cc05eb9a973895d
Mateusz <zawadzkimat@outlook.com>
2024-02-14 17:12:03 +0100

    moved to legacy

--- data/templates/.sql/trigger_update_input.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER IF NOT EXISTS update_input_iteration
-    AFTER INSERT
-    ON input_files
-    FOR EACH ROW
-BEGIN
-    UPDATE input_files
-    SET iteration_no = (SELECT COUNT(*) FROM input_files WHERE model_id = NEW.model_id)
-    WHERE id = NEW.id;
-END;
--- data/templates/.sql/trigger_update_output.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER IF NOT EXISTS update_output_iteration
-    AFTER INSERT
-    ON output_files
-    FOR EACH ROW
-BEGIN
-    UPDATE output_files
-    SET iteration_no = (SELECT COUNT(*) FROM output_files WHERE model_id = NEW.model_id)
-    WHERE id = NEW.id;
-END;
