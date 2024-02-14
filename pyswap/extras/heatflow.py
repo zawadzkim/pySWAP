ea7ce47802560eb5388b3df77b0c2830c7ae1889
Mateusz <zawadzkimat@outlook.com>
2024-02-14 16:51:14 +0100

    Initial commit.

---
+++ pyswap/extras/heatflow.py
@@ -0,0 +1,21 @@
+"""Heat flow"""
+from dataclasses import dataclass
+from pyswap.base.dtypes import Subsection
+from pandas import DataFrame
+
+
+@dataclass
+class HeatFlow(Subsection):
+
+    swhea: bool
+    swcalt: int | None = None
+    tampli: float | None = None
+    tmean: float | None = None
+    timref: float | None = None
+    ddamp: float | None = None
+    swtopbhea: int | None = None
+    tsoilfile: str | None = None
+    swbotbhea: int | None = None
+    soiltextures: DataFrame | None = None
+    initsoil: DataFrame | None = None
+    bbctsoil: DataFrame | None = None
---
+++ pyswap/extras/solutetransport.py
@@ -0,0 +1,33 @@
+"""Solute transport."""
+
+from dataclasses import dataclass
+from pyswap.base.dtypes import Subsection
+from pandas import DataFrame
+
+
+@dataclass
+class SoluteTransport(Subsection):
+
+    swsolu: bool
+    cpre: float | None = None
+    cdrain: float | None = None
+    swbotbc: int | None = None
+    cseep: float | None = None
+    ddif: float | None = None
+    tscf: float | None = None
+    swsp: bool | None = None
+    frexp: float | None = None
+    cref: float | None = None
+    swdc: bool | None = None
+    gampar: float | None = None
+    rtheta: float | None = None
+    bexp: float | None = None
+    swbr: bool | None = None
+    daquif: float | None = None
+    poros: float | None = None
+    kfsat: float | None = None
+    decsat: float | None = None
+    cdraini: float | None = None
+    cseeparrtb: DataFrame | None = None
+    inissoil: DataFrame | None = None
+    miscellaneous: DataFrame | None = None
