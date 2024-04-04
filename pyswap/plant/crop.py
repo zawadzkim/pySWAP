30f3e73debd318e2db402987a756806f833256ea
Mateusz <zawadzkimat@outlook.com>
2024-04-04 16:02:44 +0200

    Implemented save_crop()

--- pyswap/plant/crop.py
+++ pyswap/plant/crop.py
@@ -1,7 +1,8 @@
-from typing import Optional, List
+from typing import Optional, List, Literal
 from ..core.utils.files import open_file
 from ..core.utils.basemodel import PySWAPBaseModel
 from ..core.utils.fields import Table
+from ..core.utils.files import save_file
 from pydantic import computed_field, Field
 
 
@@ -18,15 +19,29 @@ class CropFile(PySWAPBaseModel):
 class Crop(PySWAPBaseModel):
     """Holds the crop settings of the simulation."""
 
-    swcrop: bool
+    swcrop: Literal[0, 1]
     rds: Optional[float] = Field(default=None, ge=1, le=5000)
     table_croprotation: Optional[Table] = None
     cropfiles: Optional[List[CropFile]] = None
 
+    @property
+    def exclude(self) -> set:
+        return {'cropfiles'}
+
     def _validate_crop_section(self):
-        if self.swcrop:
+        if self.swcrop == 1:
             assert self.rds is not None, "rds must be specified if swcrop is True"
             assert self.table_croprotation is not None, "croprotation must be specified if swcrop is True"
 
     def save_crop(self, path: str):
-        return NotImplemented('Method not implemented yet')
+        count = 0
+        for cropfile in self.cropfiles:
+            count += 1
+            save_file(
+                string=cropfile.content,
+                extension='crp',
+                fname=cropfile.name,
+                path=path,
+                mode='w'
+            )
+        return f'{count} crop file(s) saved successfully.'
