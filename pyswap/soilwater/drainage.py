cfdaf42d4ade43d49a98892fd4d13ff3bc5fe0fe
Mateusz <zawadzkimat@outlook.com>
2024-04-08 12:02:49 +0200

    excluded the drainagefile from the string representation and altered the save method.

--- pyswap/soilwater/drainage.py
+++ pyswap/soilwater/drainage.py
@@ -1,7 +1,7 @@
-from ..core.utils.files import open_file
+from ..core.utils.files import open_file, save_file
 from ..core.utils.basemodel import PySWAPBaseModel
 from pydantic import computed_field, model_validator
-from typing import Literal, Optional
+from typing import Literal, Optional, Any
 
 
 class DrainageFile(PySWAPBaseModel):
@@ -19,6 +19,11 @@ class LateralDrainage(PySWAPBaseModel):
 
     swdra: Literal[0, 1, 2]
     drfil: Optional[str] = None
+    drainagefile: Optional[Any] = None
+
+    @property
+    def exclude(self) -> set:
+        return {'drainagefile'}
 
     @model_validator(mode='after')
     def _validate_lateral_drainage(self):
@@ -26,4 +31,11 @@ class LateralDrainage(PySWAPBaseModel):
             assert self.drfil is not None, "drfil is required when swdra is 1 or 2"
 
     def save_drainage(self, path: str):
-        return NotImplemented('Method not implemented yet')
+        save_file(
+            string=self.drainagefile.content,
+            extension='dra',
+            fname=self.drainagefile.name,
+            path=path,
+            mode='w'
+        )
+        return '.dra file saved successfully.'
