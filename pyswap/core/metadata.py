0d28f789742e8b953856f3d262492e0ec190f715
Mateusz <zawadzkimat@outlook.com>
2024-04-20 17:08:40 +0200

    Switched to Field(exclude=True) instead of custom property, added docstring.

--- pyswap/core/metadata.py
+++ pyswap/core/metadata.py
@@ -1,17 +1,18 @@
+"""
+metadata.py contains the Metadata class collecting basic information about the model.
+"""
+
 from .utils.basemodel import PySWAPBaseModel
 from typing import Optional
+from pydantic import Field
 
 
 class Metadata(PySWAPBaseModel):
     """Metadata of a SWAP model."""
 
-    author: str
-    institution: str
-    email: str
+    author: str = Field(exclude=True)
+    institution: str = Field(exclude=True)
+    email: str = Field(exclude=True)
     project: str
-    swap_ver: str
-    comment: Optional[str] = None
-
-    @property
-    def include(self) -> set:
-        return {'project'}
+    swap_ver: str = Field(exclude=True)
+    comment: Optional[str] = Field(default=None, exclude=True)
