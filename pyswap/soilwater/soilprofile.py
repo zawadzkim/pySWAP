fda673181580b2f4f55644d5000307c9db7003b1
Mateusz <zawadzkimat@outlook.com>
2024-06-10 20:42:47 +0200

    Added docs, improved validation.

--- pyswap/soilwater/soilprofile.py
+++ pyswap/soilwater/soilprofile.py
@@ -5,13 +5,34 @@ from typing import Literal, Optional
 
 
 class SoilProfile(PySWAPBaseModel):
-    """Vertical discretization of soil profile, soil hydraulic functions and hysteresis of soil water retention."""
-    table_soilprofile: Table
+    """Vertical discretization of soil profile, soil hydraulic functions and hysteresis of soil water retention.
+
+    Covers parts 4, 5, 6 and 7 of the .swp file.
+
+    Attributes:
+        swsophy (Literal[0, 1]): Switch for analytical functions or tabular input
+
+            * 0 - Analytical functions with input of Mualem - van Genuchten parameters
+            * 1 - Soil physical tables
+
+        swhyst (Literal[0, 1, 2]): Hysteresis of soil water retention function
+
+            * 0 - No hysteresis
+            * 1 - Hysteresis, initial conditions wetting
+            * 2 - Hysteresis, initial conditions drying
+
+        filenamesophy (Optional[str]): Names of input files with soil hydraulic tables for each soil layer
+        tau (Optional[float]): Minimum pressure head difference to change wetting-drying
+        swmacro (Literal[0, 1]): Switch for preferential flow due to macropores
+        table_soilprofile (Table): Table with soil profile data
+        table_soilhydrfunc (Optional[Table]): Table with soil hydraulic functions
+    """
     swsophy: Literal[0, 1]
     swhyst: Literal[0, 1, 2]
     swmacro: Literal[0, 1]
     filenamesophy: Optional[str] = None
     tau: Optional[float] = None
+    table_soilprofile: Table
     table_soilhydrfunc: Optional[Table] = None
 
     @model_validator(mode='after')
@@ -24,11 +45,16 @@ class SoilProfile(PySWAPBaseModel):
                 assert column in self.table_soilprofile.columns, f"{column} is required in soilprofile"
 
         if self.swsophy == 0:
-            required_columns_hydraulic_functions = [
-                "ORES", "OSAT", "ALFA", "NPAR", "KSATFIT", "LEXP", "ALFAW", "H_ENPR", "KSATEXM", "BDENS"]
-            assert self.table_soilhydrfunc is not None, "soilhydrfunc is required when swsophy is True"
+            assert self.table_soilhydrfunc is not None, "table_soilhydrfunc is required when swsophy is True"
+            if self.swhyst == 0:
+                required_columns_hydraulic_functions = [
+                    "ORES", "OSAT", "ALFA", "NPAR", "KSATFIT", "LEXP", "H_ENPR", "KSATEXM", "BDENS"]
+            elif self.swhyst in range(1, 3):
+                required_columns_hydraulic_functions = [
+                    "ORES", "OSAT", "ALFA", "NPAR", "KSATFIT", "LEXP", "ALFAW", "H_ENPR", "KSATEXM", "BDENS"]
+
             for column in required_columns_hydraulic_functions:
-                assert column in self.table_soilhydrfunc.columns, f"{column} is required in soilhydrfunc"
+                assert column in self.table_soilhydrfunc.columns, f"{column} is required in table_soilhydrfunc"
 
         else:
             assert self.filenamesophy is not None, "filenamesophy is required when swsophy is True"
