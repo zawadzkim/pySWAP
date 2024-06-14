1089295326d78b493003c41e52ae68e068b5a6b1
Mateusz <zawadzkimat@outlook.com>
2024-06-14 13:15:56 +0200

    validation of the dataframes now handled by pandera

--- pyswap/soilwater/soilprofile.py
+++ pyswap/soilwater/soilprofile.py
@@ -38,26 +38,9 @@ class SoilProfile(PySWAPBaseModel):
     @model_validator(mode='after')
     def _validate_soil_profile(self) -> None:
 
-        if self.table_soilprofile is not None:
-            required_columns_soil_profile = [
-                "ISUBLAY", "ISOILLAY", "HSUBLAY", "HCOMP", "NCOMP"]
-            for column in required_columns_soil_profile:
-                assert column in self.table_soilprofile.columns, f"{column} is required in soilprofile"
-
         if self.swsophy == 0:
             assert self.table_soilhydrfunc is not None, "table_soilhydrfunc is required when swsophy is True"
-            if self.swhyst == 0:
-                required_columns_hydraulic_functions = [
-                    "ORES", "OSAT", "ALFA", "NPAR", "KSATFIT", "LEXP", "H_ENPR", "KSATEXM", "BDENS"]
-            elif self.swhyst in range(1, 3):
-                required_columns_hydraulic_functions = [
-                    "ORES", "OSAT", "ALFA", "NPAR", "KSATFIT", "LEXP", "ALFAW", "H_ENPR", "KSATEXM", "BDENS"]
-
-            for column in required_columns_hydraulic_functions:
-                assert column in self.table_soilhydrfunc.columns, f"{column} is required in table_soilhydrfunc"
-
         else:
             assert self.filenamesophy is not None, "filenamesophy is required when swsophy is True"
-
         if self.swhyst in range(1, 3):
             assert self.tau is not None, "tau is required when swhyst is 1 or 2"
