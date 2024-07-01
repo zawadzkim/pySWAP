f874b24d52d1a4724d3ed588cbfbbb348fa696bc
Mateusz <zawadzkimat@outlook.com>
2024-07-01 18:00:40 +0200

    begun implementing more tests

--- pyswap/testcase/run_testcase.py
+++ pyswap/testcase/run_testcase.py
@@ -1,11 +1,21 @@
 from .hupselbrook import _make_hupselbrook
+from .oxygenstress import _make_oxygenstress
+from .grassgrowth import _make_grassgrowth
+from .macroporeflow import _make_macroporeflow
+from .salinitystress import _make_salinitystress
+from .surfacewater import _make_surfacewater
 from typing import Literal
 
 
-def get(case: Literal['hupselbrook']):
-    if case == 'hupselbrook':
-        model = _make_hupselbrook()
-    else:
-        raise ValueError(f'provided case ({case}) is not available.')
+def get(case: Literal['hupselbrook', 'oxygenstress', 'grassgrowth', 'macroporeflow', 'salinitystress', 'surfacewater']):
 
-    return model
+    cases = {
+        'hupselbrook': _make_hupselbrook,
+        'oxygenstress': _make_oxygenstress,
+        'grassgrowth': _make_grassgrowth,
+        'macroporeflow': _make_macroporeflow,
+        'salinitystress': _make_salinitystress,
+        'surfacewater': _make_surfacewater
+    }
+
+    return cases[case]()
