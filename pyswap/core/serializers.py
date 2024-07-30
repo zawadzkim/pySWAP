b2ac783b855dbb83830a995ef6682ca27e7b24d5
zawadzkim <zawadzkimat2@gmail.com>
2024-07-30 16:11:59 +0200

    Replaced the quote string and is_scientific_notation with the custom String field type

--- pyswap/core/serializers.py
+++ pyswap/core/serializers.py
@@ -1,18 +1,16 @@
-"""Functions serializing obects to the appropriate format used in the custom pyswap fields 
-(pyswap.core.utils.fields)
+"""Functions serializing obects to the appropriate format used in the custom
+pyswap fields (pyswap.core.utils.fields)
 """
 
 from pandas import DataFrame, DatetimeIndex
-import re
 
 
 def serialize_table(table: DataFrame) -> str:
-    """Convert the DataFrame to a string with the headers in uppercase.
+    """Convert the DataFrame to a string.
 
-    Args:
+    Arguments:
         table: The DataFrame to be serialized.
     """
-    table.columns = [header.upper() for header in table.columns]
     return f'{table.to_string(index=False)}\n'
 
 
@@ -39,41 +37,13 @@ def serialize_csv_table(table: DataFrame) -> str:
         table['MM'] = table.index.month
         table['YYYY'] = table.index.year
         required_order = ['Station', 'DD', 'MM', 'YYYY', 'RAD',
-                          'Tmin', 'Tmax', 'HUM', 'WIND', 'RAIN', 'ETref', 'WET']
+                          'Tmin', 'Tmax', 'HUM', 'WIND', 'RAIN',
+                          'ETref', 'WET']
         table = table[required_order]
 
     table.loc[:, 'Station'] = table.Station.apply(
         lambda x: f"'{x}'" if not str(x).startswith("'") else x)
     return table.to_csv(index=False, lineterminator='\n')
-
-
-def is_scientific_notation(s: str) -> str:
-    """Check if a string represents a number in scientific notation.
-
-    The pattern matches strings in scientific notation, e.g., '1.23e-4', '2E+2'
-
-    Args:
-        s: The string to be checked.
-    """
-    pattern = r'^[+-]?(\d+(\.\d*)?|\.\d+)[eE][+-]?\d+$'
-    return re.match(pattern, s) is not None
-
-
-def quote_string(string) -> str:
-    """Quote the string if it contains alphabetic characters or './', except for scientific notation."""
-
-    string = str(string)
-
-    # Check for scientific notation first
-    if is_scientific_notation(string):
-        return string.upper()
-
-    if re.search("[a-zA-Z/]", string):
-        return f"'{string}'"
-    if re.search(r".\\", string):
-        return f"'{string}'"
-    else:
-        return string
 
 
 def serialize_object_list(list) -> str:
