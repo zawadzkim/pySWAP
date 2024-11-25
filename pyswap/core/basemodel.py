cd6293d3dbeec9462a6ed7598494a25c5af1362d
Mateusz <zawadzkimat@outlook.com>
2024-11-25 15:52:57 +0000

    refactor: moved both base models together.

--- pyswap/core/basemodel.py
+++ pyswap/core/basemodel.py
@@ -1,3 +1,22 @@
+"""
+# Base models
+
+A lot of functionality can be abstracted away in the base models. This way, the
+code is more DRY and easier to maintain. The base models are used to enforce
+the correct data types and structure of the input data. They also provide
+methods to convert the data to the format required by the SWAP model.
+
+This module defines the base model for regular classes and classes that
+validate pandas DataFrames.
+
+Classes:
+    BaseModel: Base class for pySWAP models. Inherit on Pydantic BaseModel.
+    BaseTableModel: Base class for pySWAP models that validate pandas
+        DataFrames. Inherit on Pandera DataFrameModel.
+"""
+
+
+
 from __future__ import annotations
 
 import pandas as pd
@@ -7,27 +26,24 @@ from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 
 class PySWAPBaseModel(BaseModel):
-    """Base class for PySWAP models.
+    """Base class for pySWAP models.
 
     Attributes:
         model_config (ConfigDict): Overriding Pydantic model configuration.
 
     Methods:
-        save_element: Saves model element to a file.
         model_string: Returns a custom model string representation that
             matches the requirements of .swp file.
         _concat_sections: Concatenate a string from individual sections.
-        model_string: Returns a custom model string representation that
-            matches the requirements of .swp file.
     """
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True, validate_assignment=True, extra="forbid"
     )
 
-    name: Optional[str] = Field(default="", exclude=True)
+    name: str | None = Field(default="", exclude=True)
 
-    @model_validator(mode='after')
+    @model_validator(mode="after")
     def set_name(cls, values):
         if not values.name:
             values.name = cls.__name__.lower()
@@ -76,3 +92,21 @@ class PySWAPBaseModel(BaseModel):
                 continue
             string += v.model_string()
         return string
+
+
+class BaseTableModel(pa.DataFrameModel):
+    """Base model for pandas DataFrames.
+    
+    Methods:
+        create: Create a validated DataFrame from a dictionary.
+    """
+
+    class Config:
+        coerce = True
+
+    @classmethod
+    def create(cls, data: dict) -> DataFrame:
+        df = pd.DataFrame(data)
+        df.columns = df.columns.str.upper()
+        validated_df = cls.validate(df)
+        return validated_df
