ecaac3b46a967dcfee1aa536284b3ac0eee0bf03
Mateusz <zawadzkimat@outlook.com>
2024-12-02 20:26:01 +0000

    feat: eliminate dependence on the field name in serialization

--- pyswap/core/basemodel.py
+++ pyswap/core/basemodel.py
@@ -25,6 +25,7 @@ from pandera.typing import DataFrame
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 
 
+
 class PySWAPBaseModel(BaseModel):
     """Base class for pySWAP models.
 
@@ -48,50 +49,6 @@ class PySWAPBaseModel(BaseModel):
         if not values.name:
             values.name = cls.__name__.lower()
         return values
-
-    def model_string(self) -> str:
-        """Returns a custom model string representation that matches the
-        requirements of .swp file.
-
-        Note:
-            If values are simple types, they are formatted as 'ATTR = VALUE'.
-            If the valies are tables (in pySWAP pd.DataFrame are used), they
-            are formatted simply as 'TABLE_VALUE'. Additionally, a custom
-            serializer (pyswap.core.utils.serializers.quote_string)
-            is used to quote strings
-        Returns:
-            str: Custom model string representation.
-        """
-        string = ""
-
-        def formatter(attr, value, string):
-            if attr.startswith("table_") or attr.startswith("list_"):
-                return string + value
-            else:
-                return string + f"{attr.upper()} = {value}\n"
-
-        for attr, value in self.model_dump(mode="json", exclude_none=True).items():
-            if isinstance(value, dict):
-                for k, v in value.items():
-                    string = formatter(k, v, string)
-            else:
-                string = formatter(attr, value, string)
-
-        return string
-
-    def _concat_sections(self) -> str:
-        """Concatenate a string from individual sections.
-
-        This method is meant to be used on models that collect other
-        models, like DraFile, or Model.
-        """
-
-        string = ""
-        for k, v in dict(self).items():
-            if v is None or isinstance(v, str):
-                continue
-            string += v.model_string()
-        return string
 
 
 class BaseTableModel(pa.DataFrameModel):
