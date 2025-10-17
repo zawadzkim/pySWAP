41dcc7ef1e981a5a539072a1af5ab14c71e91b95
zawadzkim <zawadzkimat@outlook.com>
2025-10-17 12:41:56 +0000

    fix: properly initiate poetry env and fix jupyter not launching.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,11 @@
 git config --global --add safe.directory /workspaces/$(basename $(pwd))
 
 pipx install poetry
+poetry config virtualenvs.in-project true
 poetry install
+
+# Install the Jupyter kernel for this environment
+poetry run python -m ipykernel install --user --name=pyswap --display-name "Python (pyswap)"
 
 # Install pre-commit hooks
 poetry run pre-commit install --install-hooks
